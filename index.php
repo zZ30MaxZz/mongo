09360cf5f3c58199bd7028d90b4f0493e303f559
Max Aguilar <zz30.maxzz@gmail.com>
2017-08-06 20:50:54 -0500

    Mostrando registros de la base de datos.

--- index.php
+++ index.php
@@ -57,6 +57,40 @@
 </form>
 <div>
     Salida
+    <?php
+    //$llave = $_POST["llave"]; //Cuando enviemos
+    //$descripcion = $_POST["descripcion"]; //Cuando enviemos
+
+    $uname = "dbgamemax";
+    $pass = "dbgamemax123";
+    $dbName = "dbgame";
+    $uri = "mongodb://".$uname.":".$pass."@ds127983.mlab.com:27983/dbgame";
+    $collection = "foo";
+
+    $conn = new MongoClient($uri);
+
+    echo ('Nuestra base de datos en mLAB:');
+    echo $conn; // para asegurarse que la conexión esté bien.
+    echo ('<br>');
+
+
+
+    $db = $conn->selectDB($dbName);
+    //echo $db;
+
+    $col = new MongoCollection($db, $collection);
+    $cur = $col->find();
+
+    $result = array();
+    foreach ($cur as $doc) {
+        $tmp = array();
+        $tmp["a"] = $doc["a"];
+        array_push($result, $tmp);
+    }
+
+    echo json_encode($result);
+    //header("Location:index.php"); //Solo lo activaremos en casos de pruebas.
+    ?>
 </div>
 <footer>
     <div class="footer">
@@ -65,37 +99,3 @@
 </footer>
 </body>
 </html>
-<?php
-//$llave = $_POST["llave"]; //Cuando enviemos
-//$descripcion = $_POST["descripcion"]; //Cuando enviemos
-
-$uname = "dbgamemax";
-$pass = "dbgamemax123";
-$dbName = "dbgame";
-$uri = "mongodb://".$uname.":".$pass."@ds127983.mlab.com:27983/dbgame";
-$collection = "foo";
-
-$conn = new MongoClient($uri);
-
-echo ('Nuestra base de datos en mLAB:');
-echo $conn; // para asegurarse que la conexión esté bien.
-echo ('<br>');
-
-
-
-$db = $conn->selectDB($dbName);
-//echo $db;
-
-$col = new MongoCollection($db, $collection);
-$cur = $col->find();
-
-$result = array();
-foreach ($cur as $doc) {
-    $tmp = array();
-    $tmp["a"] = $doc["a"];
-    array_push($result, $tmp);
-}
-
-echo json_encode($result);
-//header("Location:index.php"); //Solo lo activaremos en casos de pruebas.
-?>
