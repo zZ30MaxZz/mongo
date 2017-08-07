ad8dd4dc01c4b4282e1aed51d618b5db2a6cd518
Max Aguilar <zz30.maxzz@gmail.com>
2017-08-07 00:04:50 -0500

    Restaurando salida.

--- index.php
+++ index.php
@@ -67,13 +67,29 @@
     $uri = "mongodb://".$uname.":".$pass."@ds127983.mlab.com:27983/dbgame";
     $collection = "foo";
 
-    $conn = new MongoDB\Client($uri);
+    echo ($uri);
+    $conn = new MongoClient($uri);
 
     echo ('Nuestra base de datos en mLAB:');
     echo $conn; // para asegurarse que la conexión esté bien.
     echo ('<br>');
 
 
+
+    $db = $conn->selectDB($dbName);
+    //echo $db;
+
+    $col = new MongoCollection($db, $collection);
+    $cur = $col->find();
+
+    $result = array();
+    foreach ($cur as $doc) {
+        $tmp = array();
+        $tmp["a"] = $doc["a"];
+        array_push($result, $tmp);
+    }
+
+    echo json_encode($result);
     //header("Location:index.php"); //Solo lo activaremos en casos de pruebas.
     ?>
 </div>
