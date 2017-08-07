d08ebc22901d4d9020bb70bb7c7ed7f4185ab222
Max Aguilar <zz30.maxzz@gmail.com>
2017-08-07 00:02:07 -0500

    Prrueba

--- index.php
+++ index.php
@@ -67,28 +67,13 @@
     $uri = "mongodb://".$uname.":".$pass."@ds127983.mlab.com:27983/dbgame";
     $collection = "foo";
 
-    $conn = new MongoClient(MONGODB_URI);
+    $conn = new MongoDB\Client($uri);
 
     echo ('Nuestra base de datos en mLAB:');
     echo $conn; // para asegurarse que la conexión esté bien.
     echo ('<br>');
 
 
-
-    $db = $conn->selectDB($dbName);
-    //echo $db;
-
-    $col = new MongoCollection($db, $collection);
-    $cur = $col->find();
-
-    $result = array();
-    foreach ($cur as $doc) {
-        $tmp = array();
-        $tmp["a"] = $doc["a"];
-        array_push($result, $tmp);
-    }
-
-    echo json_encode($result);
     //header("Location:index.php"); //Solo lo activaremos en casos de pruebas.
     ?>
 </div>
